fa00201cdcf396804116b90167a020a7aa38d63e
JanZinch <jan.zinchanka@mail.ru>
2023-12-19 17:23:21 +0300

    init

---
+++ Assets/Scripts/Jobs/DetectNearbyEntitiesJob.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Jobs;
+using UnityEngine;
+
+namespace Jobs
+{
+    public struct DetectNearbyEntitiesJob : IJobParallelFor
+    {
+        public float DetectionRadius { get; private set; }
+
+        public DetectNearbyEntitiesJob(float detectionRadius)
+        {
+            DetectionRadius = detectionRadius;
+            
+            Debug.Log("Rad: " + DetectionRadius);
+        }
+
+        public void Execute(int index)
+        {
+            Debug.Log("Detect");
+        }
+    }
+}
---
+++ Assets/Scripts/Jobs/MotionJob.cs
@@ -0,0 +1,24 @@
+﻿using Unity.Collections;
+using UnityEngine;
+using UnityEngine.Jobs;
+
+namespace Jobs
+{
+    public struct MotionJob : IJobParallelForTransform
+    {
+        private NativeArray<Vector3> _positions;
+
+        public MotionJob(NativeArray<Vector3> positions)
+        {
+            _positions = positions;
+        }
+
+        public void Execute(int index, TransformAccess transform)
+        {
+            transform.position = Vector3.one; // Random.insideUnitSphere * 5.0f;
+            _positions[index] = transform.position;
+            
+            Debug.Log("Go");
+        }
+    }
+}
