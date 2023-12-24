2586fb187310b923d4b36038470553af131c596b
JanZinch <jan.zinchanka@mail.ru>
2023-12-24 23:05:47 +0300

    feat

--- Assets/Scripts/BoidsLogic/PointOfInterestJob.cs
+++ Assets/Scripts/BoidsLogic/PointOfInterestJob.cs
@@ -11,7 +11,9 @@ namespace BoidsLogic
         private AccelerationWeights _weights;
         private Vector3 _interestPosition;
 
-        private NativeArray<Vector3> _pointsOfInterest;
+        private const float ConsumeRadius = 0.5f;
+
+        [NativeDisableParallelForRestriction] private NativeArray<Vector3> _pointsOfInterest;
 
         public PointOfInterestJob(NativeArray<Vector3> positions, NativeArray<Vector3> accelerations, 
             AccelerationWeights weights, Vector3 interestPosition, NativeArray<Vector3> pointsOfInterest)
@@ -28,11 +30,27 @@ namespace BoidsLogic
             /*Vector3 acceleration = _interestPosition - _positions[index];
             _accelerations[index] += _weights.PointOfInterest * acceleration;*/
 
+            float minDistance = float.MaxValue;
+            int chosePointIndex = 0;
+            
             for (int i = 0; i < _pointsOfInterest.Length; i++)
             {
+                float distanceToPoint = Vector3.Distance(_positions[index], _pointsOfInterest[i]);
                 
+                if (distanceToPoint < minDistance)
+                {
+                    minDistance = distanceToPoint;
+                    chosePointIndex = i;
+                }
             }
-            
+
+            if (Vector3.Distance(_positions[index], _pointsOfInterest[chosePointIndex]) < ConsumeRadius)
+            {
+                Debug.Log("Consumed");
+            }
+
+            Vector3 acceleration = _pointsOfInterest[chosePointIndex] - _positions[index];
+            _accelerations[index] += _weights.PointOfInterest * acceleration;
             
         }
         
