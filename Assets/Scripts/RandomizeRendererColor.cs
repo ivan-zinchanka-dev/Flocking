f3f4964e68fe1cd774d324b9476b21c5bdb07cff
JanZinch <jan.zinchanka@mail.ru>
2023-12-25 19:11:26 +0300

    refactor: visualization

---
+++ Assets/Scripts/RandomizeRendererColor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RandomizeRendererColor : MonoBehaviour
+{
+    [SerializeField] private Renderer _renderer;
+    [SerializeField] private Color _min;
+    [SerializeField] private Color _max;
+    
+    private void Awake()
+    {
+        Color.RGBToHSV(_min, out var minH, out var minS, out var minV);
+        Color.RGBToHSV(_max, out var maxH, out var maxS, out var maxV);
+        
+        Color randomColor = Random.ColorHSV(minH, maxH, minS, maxS, minV, maxV);
+        _renderer.material.color = randomColor;
+    }
+}
